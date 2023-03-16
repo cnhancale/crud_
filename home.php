ad826f2e38f7c91cf2c876621a111aa3351c00b3
cnhancale <clnhancale@gmail.com>
2023-03-16 15:28:33 +0200

    Add Santos Template

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+include_once("header.php");  ?>
+
+
+<div class="body flex-grow-1 px-3">
+    <div class="container-lg">
+        <h1> Conteudo da VIEW HOME</h1>
+    </div>
+</div>
+
+
+<?php include_once("footer.php");  ?>
---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+include_once("header.php");  ?>
+
+
+<div class="body flex-grow-1 px-3">
+    <div class="container-lg">
+        <h1> Conteudo da VIEW Test2</h1>
+
+        <h1> Bem vindo ...</h1>
+    </div>
+</div>
+
+
+<?php include_once("footer.php");  ?>
